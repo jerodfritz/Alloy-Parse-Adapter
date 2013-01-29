d13f82c51334a4588483dfbb9891a9a9ac9d1fa7
Aaron Saunders <aaronksaunders@gmail.com>
2013-01-28 20:49:05 -0500

    plain REST API adapter for Parse.com support

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,86 +1,104 @@
+//
+// One  way to set the headers, see the model file for another
+//
 var debug_responses = {
-  success : function(response) {
-    Ti.API.info('success: ' + JSON.stringify(response));
-  },
-  error : function(response) {
-    Ti.API.info('error: ' + JSON.stringify(response));
-  }
+    success : function(response) {
+        debugger;
+        Ti.API.info('success: ' + JSON.stringify(response));
+    },
+    error : function(response) {
+        Ti.API.info('error: ' + JSON.stringify(response));
+    }
 };
+
+// add the auth headers, move to model
+_.extend(debug_responses, {
+    headers : Alloy.createCollection('Book').config.settings.headers
+});
 
 $.index.open();
 
 $.table.updateContent = function(collection) {
-  var rows = [];
-  for (var i = 0; i < collection.length; i++) {
-    var model = collection.at(i).attributes, title = "";
-    for (var key in model) {
-      if (key !== "id") {
-        title += model[key] + "  "
-      }
+    var rows = [];
+    for (var i = 0; i < collection.length; i++) {
+        var model = collection.at(i).attributes, title = "";
+        for (var key in model) {
+            if (key !== "id") {
+                title += model[key] + "  "
+            }
+        }
+        rows.push(Ti.UI.createTableViewRow({
+            title : title
+        }));
     }
-    rows.push(Ti.UI.createTableViewRow({
-      title : title
-    }));
-  }
-  this.setData(rows);
+    this.setData(rows);
 };
-
 
 var books = Alloy.createCollection('Book');
 
-books.on("fetch", function() {
-  $.table.updateContent(books);
+books.on("sync fetch change", function() { debugger;
+    $.table.updateContent(books);
 });
 
-books.fetch(debug_responses); // Freshen Table
-
+// add aquery parameter
+params = debug_responses;
+params["data"] = {
+    "where" : JSON.stringify({
+        "book" : "Time Book"
+    })
+};
+books.fetch(params);
+// Freshen Table
 
 var book = Alloy.createModel('Book', {
-  book : "Jungle Book",
-  author : "Kipling"
+    "objectId" : "sIawhytsmr"
 });
-book.save(debug_responses);
+//book.fetch(debug_responses);
+
+//book.save({},debug_responses);
 Ti.API.info(JSON.stringify(book));
 
-books.fetch(debug_responses); // Freshen Table
-book.save(_.extend({
-  author : 'Kipling II'
-},debug_responses));
+//books.fetch(debug_responses);
+// Freshen Table
+//book.save(_.extend({
+//    author : 'Kipling II'
+//}, debug_responses));
 
-books.fetch(debug_responses); // Freshen Table
+//books.fetch(debug_responses);
+// Freshen Table
 /*
-// Now we can add items to the model.
-var book = Alloy.createModel('Book', {
-  book : "Jungle Book",
-  author : "Kipling"
-});
-books.add(book);
+ // Now we can add items to the model.
+ var book = Alloy.createModel('Book', {
+ book : "Jungle Book",
+ author : "Kipling"
+ });
+ books.add(book);
 
-// Use Backbone shortcut to create a model and add to collection in single step. Does the same
-// thing as the creating a new model and then adding it to the collection.
-books.add({
-  book : "War and Peace",
-  author : "Tolstoy"
-});
+ // Use Backbone shortcut to create a model and add to collection in single step. Does the same
+ // thing as the creating a new model and then adding it to the collection.
+ books.add({
+ book : "War and Peace",
+ author : "Tolstoy"
+ });
 
-// Add will add models to local Backbone server but save triggers the CRUD create operation
-// causing the model to get added to the persistent store. During create an id is added to the
-// model signaling that the model has been persisted and no longer in the new state.
-books.forEach(function(model) {
-  model.save();
-});
+ // Add will add models to local Backbone server but save triggers the CRUD create operation
+ // causing the model to get added to the persistent store. During create an id is added to the
+ // model signaling that the model has been persisted and no longer in the new state.
+ books.forEach(function(model) {
+ model.save();
+ });
 
-// UPDATE - update the model save here triggers the CRUD update operation
-book.save({
-  author : "R Kipling"
-});
+ // UPDATE - update the model save here triggers the CRUD update operation
+ book.save({
+ author : "R Kipling"
+ });
 
-// Okay time to show the results. Remember this sync's local Backbone server with persistent store.
-books.fetch();
+ // Okay time to show the results. Remember this sync's local Backbone server with persistent store.
+ books.fetch();
 
-// DELETE - destroy triggers the CRUD delete operation
-for ( i = books.length - 1; i >= 0; i--) {
-  var model = books.at(i);
-  //Ti.API
-  //model.destroy();
-};*/
+ // DELETE - destroy triggers the CRUD delete operation
+ for ( i = books.length - 1; i >= 0; i--) {
+ var model = books.at(i);
+ //Ti.API
+ //model.destroy();
+ };*/
--- app/models/book.js
+++ app/models/book.js
@@ -1,31 +1,85 @@
+/**
+ * Example of a Alloy Model to support Parse.com API
+ *
+ * We have created a model framework that should support using a common REST API adapter and
+ * not a customized adapter for parse, we strive to stay true to backbone and keep the
+ * logic out of the adapter and here in the models so we
+ *
+ * Aaron K. Saunders
+ * blog.clearlyinnovative.com
+ * twitter: @aaronksaunders
+ *
+ */
 exports.definition = {
-  config : {
-    "columns" : {
-      "active" : "boolean"
-    },
-    "defaults" : {},
-    "adapter" : {
-      "type" : "parse",
-      "collection_name" : "Books",
-    },
-    "settings" : {
-      PARSE_APP_ID : 'jVG6mO9NLTOXYuJLz2ObPj2QHySQKNRXZrpoLdBv',
-      PARSE_REST_API_KEY : 'SBIZEdmAZqB3ssmpCqiywYgn5y0aOZO3yQiHxkFW',
-      PARSE_API_VERSION : '1',
-      class_name : "Books"
-    }
-  },
-  extendModel : function(Model) {
-    _.extend(Model.prototype, {
-    });
-    // end extend
-    return Model;
-  },
+    config : {
+        "columns" : {
+        },
+        "defaults" : {},
+        "adapter" : {
+            "collection_name" : "Books",
+        },
+        "settings" : {
+            headers : {
+                "X-Parse-Application-Id" : 'GRIoAKWUExfsT1q37Uyt66h4Lmx9ovvBAv2qigIw',
+                "X-Parse-REST-API-Key" : '3vt0buEpMRbIQ6p8OWVwBvgt6ZEKQN5rdjmiqmqR',
+            },
+            PARSE_API_VERSION : '1',
+            class_name : "Books"
+        },
+        _url : function(_model) {
+            var base_url = "https://api.parse.com/" + this.settings.PARSE_API_VERSION + "/classes";
+            return base_url + "/" + this.settings.class_name + "/" + (_model && _model.id || "");
 
-  extendCollection : function(Collection) {
-    _.extend(Collection.prototype, {
-    });
-    // end extend
-    return Collection;
-  }
+        }
+    },
+    /**
+     *
+     * BOOK MODEL
+     *
+     */
+    extendModel : function(Model) {
+        _.extend(Model.prototype, {
+            idAttribute : "objectId",
+            /**
+             * construct the proper URL
+             */
+            url : function() {
+                var model = this;
+                return this.config._url(model);
+            },
+            /**
+             * just a passthruu, but of we needed to post-process
+             * the data, it would be done here
+             */
+            parse : function(_response) { debugger;
+                return _response;
+            },
+        });
+        // end extend
+        return Model;
+    },
+    /**
+     *
+     * BOOK COLLECTION
+     *
+     */
+    extendCollection : function(Collection) {
+        _.extend(Collection.prototype, {
+            /**
+             * construct the proper URL
+             */
+            url : function() { debugger;
+                return this.config._url();
+            },
+            /**
+             * just a passthruu, but of we needed to post-process
+             * the data, it would be done here
+             */
+            parse : function(_response) {
+                return _response.results;
+            },
+        });
+        // end extend
+        return Collection;
+    }
 }
